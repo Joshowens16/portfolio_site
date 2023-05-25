546c455381d336181670170ef8bcdd232901f51f
Joshowens16 <joshowens16@gmail.com>
2023-05-24 20:08:27 -0700

    we begin v3

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Nav from "./Nav";
+
+function App() {
+  return (
+    <div className="app">
+      {" "}
+      <div className="gradient" />
+      {/* <Nav /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Nav.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Nav = () => {
+  return <div className="nav-bg-md w-[100%] p-4">Nav</div>;
+};
+
+export default Nav;
